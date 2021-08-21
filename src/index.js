88d9b99a4a72fcf0589bd7222a7d03a08bd10aa8
Corinne Tinacci <corinne.tinacci@gmail.com>
2021-08-21 17:26:03 -0400

    doesn't work now

--- src/index.js
+++ src/index.js
@@ -1,53 +1,5 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { render } from 'react-dom';
-import axios from 'axios';
-
-import Venues from './Venues';
-import Venue from './Venue';
-import Navbar from './Navbar';
-
-class App extends Component{
-  constructor(props){
-    super(props);
-    this.state = {
-      venues: [],
-      selectedVenue: {},
-     
-    };
-    this.venueSelected = this.venueSelected.bind(this);
-  }
-  async componentDidMount(){
-    this.setState({
-      venues: (await axios.get('/api/venues')).data,
-      
-    });
-  }
-  
-  async venueSelected (venueId){
-    if (venueId !== ''){
-      const venue = (await axios.get(`/api/venues/${venueId}`)).data;
-      this.setState({selectedVenue: venue});
-    } else {
-      this.setState({selectedVenue: venueId});
-    }
-  }
-
-  render(){
-    const { venues, selectedVenue } = this.state;
-    const {venueSelected} = this;
-    return (
-      <div id='main'>
-      {
-      <Navbar venueSelected={venueSelected} />
-      }
-        <div>
-            {
-              selectedVenue.id ? <Venue selectedVenue={selectedVenue} /> : <Venues venues={venues} venueSelected={venueSelected} />
-            }
-        </div>
-      </div>
-    );
-  }
-}
+import App from './App';
 
 render(<App />, document.querySelector('#root'));

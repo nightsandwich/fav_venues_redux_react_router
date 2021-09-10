4f5bf0fc50dcce6cb32000524fe52c5fdc77a990
Corinne Tinacci <corinne.tinacci@gmail.com>
2021-09-10 14:31:04 -0400

    heroku script

--- server/db/db.js
+++ server/db/db.js
@@ -1,5 +1,13 @@
 const Sequelize = require('sequelize');
-const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/fav_venues_db');
+const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/fav_venues_db', {
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    }
+});
 
 
 
